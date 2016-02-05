1ec1e30a5e2768c79fc26adf8a3605a208b8a083
Enrico Hoffmann <dasrick@gmail.com>
2016-02-05 15:49:01 +0100

    add mongoose stuff

--- server.js
+++ server.js
@@ -6,12 +6,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 var app = express();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // config env stuff
 // ---------------------------------------------------------------------------------------------------------------------
 var port = process.env.PORT || 6080;
+var mongoconnection = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017';
+var env = process.env.NODE_ENV || 'development';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // config app
@@ -26,6 +29,11 @@ app.use(bodyParser.json());
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // MONGOOSE
 // ---------------------------------------------------------------------------------------------------------------------
+//mongoose.connect(mongoconnection); // connect to our database
+function connect() {
+  var options = {server: {socketOptions: {keepAlive: 1}}};
+  return mongoose.connect(mongoconnection, options).connection;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // MODELS
@@ -40,7 +48,7 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function (req, res, next) {
   // do logging
-  console.log('Something is happening.');
+  console.log('something is going on...');
   next();
 });
 // ---------------------------------------------------------------------------------------------------------------------
@@ -55,8 +63,21 @@ app.use('/api', router);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // START SERVER
 // ---------------------------------------------------------------------------------------------------------------------
-app.listen(app.get('port'), function () {
+//app.listen(app.get('port'), function () {
+//  console.log('Node app of WBC-API running ...');
+//  console.log('Host: localhost:' + app.get('port'));
+//  console.log('Env: ', process.env.NODE_ENV);
+//});
+
+connect()
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen);
+
+function listen() {
+  //if (app.get('env') === 'test') return;
+  app.listen(port);
   console.log('Node app of WBC-API running ...');
-  console.log('Host: localhost:' + app.get('port'));
-  console.log('Env: ', process.env.NODE_ENV);
-});
+  console.log('Host: localhost:' + port);
+  console.log('Env: ', env);
+}
